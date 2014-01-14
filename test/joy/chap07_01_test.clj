3d5d194353ed77ec7f82520112ac325732429a01
Attila Domokos <adomokos@gmail.com>
2014-01-13 21:59:12 -0600

    Looking at :pre and :post conditions in functions

--- test/joy/chap07_01_test.clj
+++ test/joy/chap07_01_test.clj
@@ -13,3 +13,15 @@
               {:band "Bill Evans" :plays 979 :loved 9}
               {:band "Eno" :plays 2333 :loved 15}]
              (sort-by-loved-ratio plays))))))
+
+(deftest named-arguments-in-clojure-test
+  (testing "with destructuring"
+    (is (= -6.0 (slope :p1 [4 15] :p2 [3 21])))))
+
+(deftest function-pre-and-post-condtion-test
+  (testing "same values"
+    (is (thrown? AssertionError (slope2 [10 10][10 10]))))
+  (testing "second argument is a list"
+    (is (thrown? AssertionError (slope2 [10 1] '(1 20)))))
+  (testing "result is not float"
+    (is (= 1.0 (slope2 [10 1] [1 20])))))
