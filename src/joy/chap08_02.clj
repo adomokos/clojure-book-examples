a2523e144d365684de1bc157da1649c7b30acd2a
Attila Domokos <adomokos@gmail.com>
2014-01-24 08:54:46 -0600

    Trying another example with unless macro

--- src/joy/chap08_02.clj
+++ src/joy/chap08_02.clj
@@ -1,6 +1,8 @@
 (ns joy.chap08-02)
 
 (defmacro unless [condition true-body false-body]
+  ;(println condition)
+  ;(println true-body)
   `(if (not ~condition)
      ~true-body
      ~false-body
@@ -10,3 +12,9 @@
   (unless (odd? number)
     :even
     :odd))
+
+(defn from-end [s n]
+  (let [delta (dec (- (count s) n))]
+    (unless (neg? delta)
+      (nth s delta)
+      nil)))
--- test/joy/chap08_02_test.clj
+++ test/joy/chap08_02_test.clj
@@ -4,6 +4,8 @@
 
 (deftest testing-the-unless-macro
   (testing "with a simple example"
-    (print (macroexpand-1 `(unless false 1 2)))
+    ;(print (macroexpand-1 `(unless false 1 2)))
     (is (= :even (odd-or-even 4)))
-    (is (= :odd (odd-or-even 3)))))
+    (is (= :odd (odd-or-even 3))))
+  (testing "10 from 100 is 90"
+    (is (= 90 (from-end (range 1 101) 10)))))
