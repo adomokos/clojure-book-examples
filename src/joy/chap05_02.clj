49cc8856d9143f6371ba8edfca0e1ae8bcefe380
Attila Domokos <adomokos@gmail.com>
2014-07-11 22:24:43 -0500

    Removing all the Joy of Clojure 1st Version examples

--- src/joy/chap05_02.clj
+++
@@ -1,8 +0,0 @@
-(ns joy.chap05-02)
-
-(defn combining-sequences [another]
-  (let [initial-vector [:a :b :c]]
-    (into initial-vector another)))
-
-(defn primitive-vectors [source-vector]
-  (into (vector-of :int) source-vector))
--- test/joy/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns joy.core-test
-  (:require [clojure.test :refer :all]
-            [joy.core :refer :all]))
-
-(deftest a-test
-  (testing "no, I am passing"
-    (is (= 1 1))))
