81ce946acc7cd163a44f0f122ef983394f912391
Attila Domokos <adomokos@gmail.com>
2014-07-31 22:06:53 -0500

    Adding more examples for quoting

--- test/joy/chap02_test.clj
+++ test/joy/chap02_test.clj
@@ -7,3 +7,12 @@
     (is (= 55 (sum-down-from 0 10))))
   (testing "using loop"
     (is (= 55 (sum-down-with-loop 10)))))
+
+(deftest quoting-test
+  (testing "unquote"
+    (is (= `(+ 10 6 2) `(+ 10 ~(* 3 2) 2)))
+    (is (= '(1 (2 3)) (let [x '(2 3)] `(1 ~x)))))
+  (testing "unquote-splicing"
+    (is (= '(1 2 3) (let [x '(2 3)] `(1 ~@x)))))
+  (testing "accessing instance members"
+    (is (= 10 (.-x (java.awt.Point. 10 20))))))
