58d41621ab2eb231ce63928aae2342d809d56703
Attila Domokos <adomokos@gmail.com>
2014-09-19 14:01:13 -0500

    Examples with the threading macro

--- src/in_action/chap02.clj
+++ src/in_action/chap02.clj
@@ -9,3 +9,25 @@
   (for [x (range 2 (inc x))
           :when (prime? x)]
     x))
+
+(defn final-amount [principle rate time-periods]
+  (* (Math/pow (+ 1 (/ rate 100)) time-periods) principle))
+
+(defn final-amount-> [principle rate time-periods]
+  (-> rate
+      (/ 100)
+      (+ 1)
+      (Math/pow time-periods)
+      (* principle)))
+
+(defn factorial [n]
+  (reduce *
+          (range 1
+                 (+ 1
+                    n))))
+
+(defn factorial->> [n]
+  (->> n
+      (+ 1)
+      (range 1)
+      (reduce *)))
--- test/in_action/chap02_test.clj
+++ test/in_action/chap02_test.clj
@@ -9,3 +9,14 @@
     (is (= '(2 3 5 7 11 13 17 19)
            (primes-less-than 20)))))
 
+(deftest the-threading-macro
+  (testing "calculating compound interest"
+    (is (= 120.0 (final-amount 100 20 1)))
+    (is (= 144.0 (final-amount 100 20 2)))
+    (is (= 120.0 (final-amount-> 100 20 1)))
+    (is (= 144.0 (final-amount-> 100 20 2)))))
+
+(deftest factorial-with-thread-last
+  (testing "the verbose implementation"
+    (is (= 120 (factorial 5)))
+    (is (= 120 (factorial->> 5)))))
