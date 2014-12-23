1bb28243d1c5273566c7e0e9e356eafe083d1648
Attila Domokos <adomokos@gmail.com>
2014-12-22 22:16:42 -0600

    Before removing duplication with higher order functions

--- test/in_action/chap08_test.clj
+++ test/in_action/chap08_test.clj
@@ -1,31 +1,31 @@
 (ns in-action.chap08-test
-  (:require [clojure.test :refer :all]
-            [in-action.chap08_01 :refer :all]))
+  (:require [clojure.test :refer :all]))
 
-(deftest test-simple-date-parsing
-  (let [date "2009-1-22"]
-    (is (= (day-from date) 22))
-    (is (= (month-from date) 1))
-    (is (= (year-from date) 2009))))
 
-(deftest test-as-string
-  (let [date "2009-1-22"]
-    (is (= (as-string date) "2009-01-22"))))
+(defn square [x]
+  (* x x))
 
-(deftest test-incrementing-date
-  (let [date-string "2009-10-31"
-        n-day (increment-day date-string)
-        n-month (increment-month date-string)
-        n-year (increment-year date-string)]
-    (is (= n-day (date "2009-11-01")))
-    (is (= n-month (date "2009-11-30")))
-    (is (= n-year (date "2010-10-31")))))
+(defn square-all [numbers]
+  (if (empty? numbers)
+    ()
+    (cons (square (first numbers))
+          (square-all (rest numbers)))))
 
-(deftest test-decrementing-date
-  (let [date-string "2009-10-31"
-        n-day (decrement-day date-string)
-        n-month (decrement-month date-string)
-        n-year (decrement-year date-string)]
-    (is (= n-day (date "2009-10-30")))
-    (is (= n-month (date "2009-09-30")))
-    (is (= n-year (date "2008-10-31")))))
+(defn cube [x]
+  (* x x x))
+
+(defn cube-all [numbers]
+  (if (empty? numbers)
+    ()
+    (cons (cube (first numbers))
+          (cube-all (rest numbers)))))
+
+
+(deftest square-them
+  (testing "square all the elements in a list"
+    (is (= '(1 4 9 16 25 36) (square-all [1 2 3 4 5 6])))
+    (is (empty? (square-all [])))))
+
+(deftest cube-them
+  (testing "cube all the elements in a list"
+    (is (= '(1 8 27 64 125 216) (cube-all [1 2 3 4 5 6])))))
