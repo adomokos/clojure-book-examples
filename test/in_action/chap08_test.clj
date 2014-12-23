6e896ed18e14dacf024b8880a561e6f1b87fe06e
Attila Domokos <adomokos@gmail.com>
2014-12-22 22:59:12 -0600

    Using a simpler  function

--- test/in_action/chap08_test.clj
+++ test/in_action/chap08_test.clj
@@ -26,3 +26,39 @@
     (is (= '(1000000000000 1000300030001)
             (take 2 (drop 10000 (do-to-all cube (range 11000))))))
     (is (empty? (do-to-all cube [])))))
+
+;(defn total-of [numbers]
+  ;(loop [nums numbers sum 0]
+    ;(if (empty? nums)
+      ;sum
+      ;(recur (rest nums) (+ sum (first nums))))))
+
+(defn larger-of [x y]
+  (if (> x y) x y))
+
+;(defn greatest-of [numbers]
+  ;(loop [l numbers candidate (first numbers)]
+    ;;(prn l numbers "-" candidate)
+    ;(if (empty? l)
+      ;candidate
+      ;(recur (rest l) (larger-of candidate (first l))))))
+
+(defn compute-accross [func elements value]
+  (if (empty? elements)
+    value
+    (recur func (rest elements) (func value (first elements)))))
+
+(defn total-of [numbers]
+  (compute-accross + numbers 0))
+
+(defn greatest-of [numbers]
+  (compute-accross larger-of  numbers (first numbers)))
+
+(deftest reducing-list-of-things
+  (testing "total of a vector of numbers"
+    (is (= 39 (total-of [5 7 9 3 4 1 2 8])))))
+
+(deftest finding-greatest-of
+  (testing "finding the largest element in a vector"
+    (is (= 9 (greatest-of [5 7 9 3 4 1 2 8])))
+    (is (nil? (greatest-of [])))))
