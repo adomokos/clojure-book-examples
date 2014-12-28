372f3d02731e701e1546dd14bf38af9ac5812e3d
Attila Domokos <adomokos@gmail.com>
2014-12-28 16:07:10 -0600

    More about closures

--- test/in_action/chap08_02_test.clj
+++ test/in_action/chap08_02_test.clj
@@ -12,3 +12,34 @@
   (testing "increment it by 2"
     (is (= 3 (inc-by-two 1)))
     (is (= 5 (inc-by-two 3)))))
+
+(defn new-user [login password email]
+  (fn [a & args]
+    (condp = a
+      :login login
+      :password password
+      :email email
+      :authenticate (= password (first args)))))
+
+(deftest closures-and-objects
+  (testing "adding a new user"
+    (let [arjun (new-user "arjun" "secret" "arjun@zololabs.com")]
+      (is (= "arjun" (arjun :login)))
+      (is (= "secret" (arjun :password)))
+      (is (= "arjun@zololabs.com" (arjun :email))))
+    (let [adi (new-user "adi" "secret" "adi@currylogic.com")]
+      (is (not (adi :authenticate "blah")))
+      (is (adi :authenticate "secret")))))
+
+(defn new-class [class-name]
+  (fn [command & args]
+    (condp = command
+      :name (name class-name))))
+(defmacro defclass [class-name]
+  `(def ~class-name (new-class '~class-name)))
+
+(defclass Person)
+
+(deftest defining-classes
+  (testing "with a macro"
+    (is (= "Person" (Person :name)))))
